56f1217a4a496c158570ce98e026485c45289ac3
degterov_a <degterov_a@lwo.by>
2019-06-05 16:18:18 +0300

    промежуточное сохранение

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,19 +1,17 @@
 var express = require('express');
-var cors = require('cors');
 var router = express.Router();
 var Chatmessage = require('../models/chatmessage.models');
 
-router.get('/', cors(), function(req, res, next) {
+router.get('/', function(req, res, next) {
     Chatmessage.findAll({
-    when: {"name": "Alexdeg"}
+    when: { }
   }).then(data => {
-     console.log(data[0]);
+     //console.log(data[0]);
     res.render('baseMsg', { data: data } ); 
     }).catch(err => {
         res.json(err);
         console.log(err);
     })
-    // res.render('baseMsg');
 });
 
 module.exports = router;
