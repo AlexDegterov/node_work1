a19eb88aa58f1962e69b2408d52d198ed11d213b
AlexDegterov <degterov@proektant.by>
2019-06-18 21:48:40 +0300

    минификация и удаление мусора только на Production

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require ('laravel-mix');
 
+// mix.combine(['resources/js/addtask.js', 'resources/js/modal.js'], 'public/javascripts/laravel.js')
+//mix.minify('resources/js/addtask.js');
 mix.combine(['resources/js/addtask.js', 'resources/js/modal.js'], 'public/javascripts/laravel.js')
     .sass('resources/css/laravel1.scss', 'resources/css/tmp')
     .sass('resources/css/laravel2.scss', 'resources/css/tmp')
